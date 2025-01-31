55dbff922c2e6546dd0480c14b84ffee7351d0b6
Youssef Abbas <youssefabbasawad@gmail.com>
2025-01-31 15:43:27 +0330

    Lesson 1

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import RegisterForm from "./RegisterForm";
+
+const RegisterPage = () => {
+    return (
+        <section className="w-2/5">
+            <div className="bg-white shadow-md rounded-md p-5">
+                <h1 className="font-bold text-3xl text-slate-700 mb-5 text-center">
+                    Create new account
+                </h1>
+                <RegisterForm />
+                <p className="p-1 mt-3">
+                    Already have an account?
+                    <Link href="/login" className="mx-1 text-blue-700 underline">
+                        Login
+                    </Link>
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default RegisterPage
