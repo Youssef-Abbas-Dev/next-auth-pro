ba8ff66a91d6e70be98e0ebae48be5834f2b083e
Youssef Abbas <youssefabbasawad@gmail.com>
2025-02-13 09:25:29 +0330

    Lesson 09

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,6 +1,7 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { prisma } from "./utils/prisma";
 import { LoginSchema } from "./utils/validationSchmas";
 import * as bcrypt from "bcryptjs";
@@ -25,6 +26,10 @@ export default {
         GitHub({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET
+        }),
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ]
 } satisfies NextAuthConfig
--- src/components/SocialProviders.tsx
+++ src/components/SocialProviders.tsx
@@ -13,7 +13,7 @@ const SocialProviders = () => {
 
   return (
     <div className="flex items-center justify-center gap-6 mt-6">
-      <div className="border bg-blue-100 hover:bg-blue-200 rounded px-4 py-2 cursor-pointer w-1/2 flex justify-center items-center">
+      <div onClick={() => socialLoginHandler("google")} className="border bg-blue-100 hover:bg-blue-200 rounded px-4 py-2 cursor-pointer w-1/2 flex justify-center items-center">
         <FcGoogle className="text-4xl" />
       </div>
       <div onClick={() => socialLoginHandler('github')} className="border bg-slate-100 hover:bg-slate-200 rounded px-4 py-2 cursor-pointer w-1/2 flex justify-center items-center">
